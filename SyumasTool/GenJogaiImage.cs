eaa75f37090520f1894b4f178f3cd16422668929
ragumaru <takorush@gmail.com>
2023-03-29 15:09:17 +0900

    除外画面生成処理実装仕掛り

---
+++ SyumasTool/GenJogaiImage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyumasTool;
+
+internal class GenJogaiImage
+{
+    class JogaiRowData
+    {
+        DataRow Row { get; }
+
+        /// <summary>除外理由</summary>
+        string ExclusionReason => Row[0].ToString() ?? "";
+
+
+        public JogaiRowData(DataRow row)
+        {
+            Row = row;
+        }
+    }
+
+    internal void Gen(string outputPath, DataTable jogaiTable) { }
+}
